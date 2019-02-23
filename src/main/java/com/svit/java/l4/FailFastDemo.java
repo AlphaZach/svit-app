fa2bb9d32fc07cb20e95de4c4197a2695391c145
zhifeng <zhifeng@berkeley.edu>
2019-02-23 11:37:44 -0800

    modify

--- src/main/java/com/svit/java/l4/FailFastDemo.java
+++ src/main/java/com/svit/java/l4/FailFastDemo.java
@@ -12,25 +12,25 @@ import java.util.Map;
  * 
  * @author sv-it
  */
-public class FailFastDemo {
-
-	public static void main(String[] args) {
-        Map<String,String> map = new HashMap<String,String>();
-        map.put("A", "1");
-        map.put("B", "2");
-        map.put("C","3");
-        
-        Iterator iterator = map.keySet().iterator();
-        
-        while (iterator.hasNext()) {
-            System.out.println(map.get(iterator.next()));
-            map.put("B", "4"); //modification causes java.util.ConcurrentModificationException
-                                //if put a entry with key "D" will cause concurrent error
-        }
-        HashSet<String, int> set = new Hashset<~>();
-        set.add("E",null);
-        Iterator iter = set.iterator();
-        System.out.println(iter.next());
-	}
-
-}
+//public class FailFastDemo {
+//
+//	public static void main(String[] args) {
+//        Map<String,String> map = new HashMap<String,String>();
+//        map.put("A", "1");
+//        map.put("B", "2");
+//        map.put("C","3");
+//
+//        Iterator iterator = map.keySet().iterator();
+//
+//        while (iterator.hasNext()) {
+//            System.out.println(map.get(iterator.next()));
+//            map.put("B", "4"); //modification causes java.util.ConcurrentModificationException
+//                                //if put a entry with key "D" will cause concurrent error
+//        }
+//        HashSet<String, Integer> set = new Hashset<String, Integer>();
+//        set.add("E",null);
+//        Iterator iter = set.iterator();
+//        System.out.println(iter.next());
+//	}
+//
+//}
--- src/main/java/com/svit/java/l6/SingletonLazyDemo.java
+++ src/main/java/com/svit/java/l6/SingletonLazyDemo.java
@@ -22,52 +22,52 @@ package com.svit.java.l6;
 //  } 
 //} 
 
-class Singleton {
-    // private static Singleton singletonObject;
-    private volatile static Singleton singletonObject; // use volatile to avoid
-                                                       // uncertainty issue by
-                                                       // compiler from double
-                                                       // check locking
-
-    // Step 1: Provide a default Private constructor ; A private Constructor
-    // prevents any other class from instantiating.
-    private Singleton() {
-        // this constructor is required to override default public constructor
-        // Implementation code is optional in private constructor,
-    }
-
-    // Step 2: Create a Method for getting the reference to the Singleton Object
-    // Step 3: Make the Access method Synchronized to prevent Thread Problems.
-    public static synchronized Singleton getSingletonObject() {
-        if (singletonObject == null) { // first lock
-            synchronized (Singleton.class) {
-                if (singletonObject == null) // double lock
-                    singletonObject = new Singleton();
-            }
-
-        }
-        return singletonObject;
-    }
-
-    // Step 4: Override the Object clone method to prevent cloning
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        throw new CloneNotSupportedException();
-    }
-}
-
-public class SingletonLazyDemo {
-
-    public static void main(String[] args) {
-         // Your Business Logic to return singleton objects, which are from same memory location
-        System.out.println("Singleton object obtained!"
-                + Singleton.getSingletonObject());
-
-        System.out.println("Singleton object obtained again!"
-                + Singleton.getSingletonObject());
-
-        System.out.println("Singleton object obtained 3rd time!"
-                + Singleton.getSingletonObject());
-
-    }
-}
+//class Singleton {
+//    // private static Singleton singletonObject;
+//    private volatile static Singleton singletonObject; // use volatile to avoid
+//                                                       // uncertainty issue by
+//                                                       // compiler from double
+//                                                       // check locking
+//
+//    // Step 1: Provide a default Private constructor ; A private Constructor
+//    // prevents any other class from instantiating.
+//    private Singleton() {
+//        // this constructor is required to override default public constructor
+//        // Implementation code is optional in private constructor,
+//    }
+//
+//    // Step 2: Create a Method for getting the reference to the Singleton Object
+//    // Step 3: Make the Access method Synchronized to prevent Thread Problems.
+//    public static synchronized Singleton getSingletonObject() {
+//        if (singletonObject == null) { // first lock
+//            synchronized (Singleton.class) {
+//                if (singletonObject == null) // double lock
+//                    singletonObject = new Singleton();
+//            }
+//
+//        }
+//        return singletonObject;
+//    }
+//
+//    // Step 4: Override the Object clone method to prevent cloning
+//    @Override
+//    public Object clone() throws CloneNotSupportedException {
+//        throw new CloneNotSupportedException();
+//    }
+//}
+//
+//public class SingletonLazyDemo {
+//
+//    public static void main(String[] args) {
+//         // Your Business Logic to return singleton objects, which are from same memory location
+//        System.out.println("Singleton object obtained!"
+//                + Singleton.getSingletonObject());
+//
+//        System.out.println("Singleton object obtained again!"
+//                + Singleton.getSingletonObject());
+//
+//        System.out.println("Singleton object obtained 3rd time!"
+//                + Singleton.getSingletonObject());
+//
+//    }
+//}
